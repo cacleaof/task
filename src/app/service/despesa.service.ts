e1116844ffb0cd9cb426929a2a83092ed533e0a3
CALEAO\caleao <cacleaof@gmail.com>
2025-06-10 20:02:34 -0300

    url vercel

--- src/app/service/despesa.service.ts
+++ src/app/service/despesa.service.ts
@@ -8,14 +8,15 @@ import { Reg } from '../model/reg';
 })
 export class DespesaService {
 
-   private apiUrl = 'http://localhost:3000/registros';
+  private apiUrl = 'https://back-json-v2ru.vercel.app/';
+  //private apiUrl = 'https://localhost:3000/';
 
   constructor(private http: HttpClient) { }
     addReg(RegData: Partial<Reg>): Observable<Reg> {
-      return this.http.post<Reg>(this.apiUrl, RegData);
+      return this.http.post<Reg>(this.apiUrl+'despesa', RegData);
     }
 
     getRegs(): Observable<Reg[]> {
-      return this.http.get<Reg[]>(this.apiUrl);
+      return this.http.get<Reg[]>(this.apiUrl+'despesas');
     }
 }
